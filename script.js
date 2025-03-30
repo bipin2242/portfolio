f75a7fdd70654f618022dcc11cb0e080b228c543
Bipin Sharma <sharmabipin024@gmail.com>
2025-03-30 13:07:37 +0545

    updated

--- script.js
+++ script.js
@@ -49,17 +49,19 @@ typeWriter();
 //   alert("Message sent! (This is a demo)");
 // });
 
-(function() {
-  emailjs.init("rRZJMpc1iV93fjQ3v"); 
+(function () {
+  emailjs.init("rRZJMpc1iV93fjQ3v");
 })();
 
 function sendMail(event) {
   event.preventDefault();
 
-  emailjs.sendForm("service_bgskdee", "template_sdqzgkw", event.target)
-  .then(() => {
+  emailjs.sendForm("service_bgskdee", "template_sdqzgkw", event.target).then(
+    () => {
       alert("Message sent successfully!");
-  }, (error) => {
+    },
+    (error) => {
       alert("Failed to send message: " + error.text);
-  });
+    }
+  );
 }
